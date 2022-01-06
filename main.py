74b5e3db676bf50d1193bd7961c82c5f38d090d3
Damian <damian.larrascq@gmail.com>
2022-01-06 03:06:44 -0300

    added font and text

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ pygame.init()
 # set screen width and height
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("First Game")
+
 # object that controls time and frames
 clock = pygame.time.Clock()
-# variable for surface
+
+# game font
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+# variables for surface
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My Game', False, 'Green')
+
 # game loop
 while True:
 
@@ -26,7 +33,10 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (350, 25))
+
     # update everything in the screen
     pygame.display.update()
+
     # tells while loop to not run faster than 60fps
     clock.tick(60)
