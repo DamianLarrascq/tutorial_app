2cf987c12dcc4603e3398e7aa0df880bfd56a558
Damian <damian.larrascq@gmail.com>
2022-01-06 01:59:25 -0300

    framerate cap

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ pygame.init()
 # set screen width and height
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("First Game")
+# object that controls time and frames
+clock = pygame.time.Clock()
 # game loop
 while True:
 
@@ -21,3 +23,5 @@ while True:
 
     # update everything in the screen
     pygame.display.update()
+    # tells while loop to not run faster than 60fps
+    clock.tick(60)
