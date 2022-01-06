7a8ebc844b0963d97afa90d425f63cf39e8a4587
Damian <damian.larrascq@gmail.com>
2022-01-06 02:48:22 -0300

    added ground surface

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ pygame.display.set_caption("First Game")
 clock = pygame.time.Clock()
 # variable for surface
 sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 # game loop
 while True:
 
@@ -24,7 +25,7 @@ while True:
             exit()
 
     screen.blit(sky_surface, (0, 0))
-
+    screen.blit(ground_surface, (0, 300))
     # update everything in the screen
     pygame.display.update()
     # tells while loop to not run faster than 60fps
